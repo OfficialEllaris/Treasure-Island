4e13f313a9fac761fcbd2d9daffc5d3a7d817137
Excel Oghenetejiri Prosper <official.ellaris@gmail.com>
2024-08-05 10:05:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+milestone_01 = input('You are at a crossroad. Where do you want to go? Type "left" or "right"').lower()
+if milestone_01 == "left":
+  milestone_02 = input('Lake with island in center. Wait for boat (type "wait") or swim (type "swim")?').lower()
+  if milestone_02 == "wait":
+    milestone_03 = input("At the island unharmed, a house with 3 doors: red, yellow, and blue. Which do you choose?").lower()
+    if milestone_03 == "red":
+      print("You are burned by fire. Game over.")
+    elif milestone_03 == "yellow":
+      print("You win!")
+    elif milestone_03 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("Game over.")
+  else:
+    print("You were attacked by trout. Game over.")
+else:
+  print("You fell into a hole. Game Over.")
